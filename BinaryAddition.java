4f69c7f70dd6a2db23bac7e88478b1d489dcf7dc
RAGUL RAJ N <152047129+RagulRajN@users.noreply.github.com>
2026-06-16 09:15:48 +0530

    4. BinaryAddition.java program

---
+++ BinaryAddition.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BinaryAddition {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input first binary number: ");
+        String binary1 = sc.next();
+
+        System.out.print("Input second binary number: ");
+        String binary2 = sc.next();
+
+        int num1 = Integer.parseInt(binary1, 2);
+        int num2 = Integer.parseInt(binary2, 2);
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum of two binary numbers: " + Integer.toBinaryString(sum));
+
+        sc.close();
+    }
+}
